d43081073caff3e804debcbdd4b52956f0f86a12
Anna Wiley <awiley33@gmail.com>
2023-05-18 10:59:36 -0500

    test: Add a second test for paint method

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -21,11 +21,14 @@ RSpec.describe Room do
       expect(room2.area).to eq(180)
     end
 
-    it "can be painted" do
+    it "can is unpainted by default" do
       room = Room.new(:bedroom, 10, '13')
 
       expect(room.is_painted?).to eq(false)
+    end
 
+    it "can be painted" do
+      room = Room.new(:bedroom, 10, '13')
       room.paint
 
       expect(room.is_painted?).to eq(true)
